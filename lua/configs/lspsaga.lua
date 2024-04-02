a17bd3748c403601ab3835185a20d2e59cb203ac
Huy Nguyen <huy55465@gmail.com>
2024-04-01 21:51:59 -0400

    feat: add description to lspsaga keymap

--- lua/configs/lspsaga.lua
+++ lua/configs/lspsaga.lua
@@ -4,4 +4,5 @@ saga.setup {
     enable = false,
   },
 }
-map("n", "<leader>ol", "<cmd>Lspsaga outline<cr>")
+map("n", "<leader>ol", "<cmd>Lspsaga outline<cr>", {desc = "Lspsaga Toggle outline"})
+map("n", "<leader>tK", "<cmd>Lspsaga peek_type_definition<cr>", {desc = "LspSaga Peek type definition"})
